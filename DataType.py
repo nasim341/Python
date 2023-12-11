3192140e260b5875b77d58374a567cf671948efd
Nasim <mdnasimahmed341@gmail.com>
2023-12-12 03:28:46 +0600

    Operators & data types

---
+++ DataType.py
@@ -0,0 +1,17 @@
+''' Data Types
+
+1.Numeric data types: int, float, complex
+
+2.String data types: str
+
+3.Sequence types: list, tuple, range
+
+4.Binary types: bytes, bytearray, memoryview
+
+5.Mapping data type: dict
+
+6.Boolean type: bool
+
+7.Set data types: set, frozenset
+
+'''

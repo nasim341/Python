a590ed35c7f4826a184d581c1c60ddcaf990dd1b
Nasim <mdnasimahmed341@gmail.com>
2023-12-13 05:10:04 +0600

    basic loop clearation

---
+++ Loop.py
@@ -0,0 +1,32 @@
+"""Available Loops in Python
+
+1. "for" = "for" loop iterate over a given sequence.
+    Such as: List, Tuples, String, etc.
+
+2. "while" = "while" loops repeat as long as certain boolean condition is met
+
+"""
+###########################For Loop Syntex####################
+''' 
+# 
+
+for <var> in <iterable>:
+   <statement(s)>
+
+<iterable> is a collection of object , for example , a list or tuple.
+<iterable(s)>in the loop body are denoted by indentation.
+<var> takes on the value of the next element in <iterable> each time through the loop. 
+
+'''
+##################While Loop Syntex################
+"""
+ while condition:
+ body of while loop
+
+"""
+
+num = 1;
+while num <= 10:
+    print(num)
+    num += 1;
+print("Nasim Aahmed")

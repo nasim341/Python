5414a712c0624b99a9b9d57adcc47115dc49fc80
Nasim <mdnasimahmed341@gmail.com>
2023-12-17 03:15:53 +0600

    string declaration

---
+++ String.py
@@ -0,0 +1,37 @@
+## Looping Through a String
+for a in "bulbul":
+    print(a)
+
+## String Length
+
+a = "Hello World"
+print(len(a))
+
+## Strings are Arrays
+a = "Python"
+print(a[5])
+
+## Check Str
+a = "Python"
+print("Python" is a)
+
+## str
+txt = "python"
+if "Python" not in txt:
+  print("No")
+
+## Slicing
+uv = "w3school"
+print(uv[2:4])
+
+## Slice From the Start
+test = "Python"
+print(test[:4])
+
+## Slice To the End
+test1 = 'practice'
+print(test1[2:])
+
+## Negative Indexing
+test2 = "Hello, World"
+print(test2[-4:-2])

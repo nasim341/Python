06efa347916321d48abb4d764cd22cfd6cf76577
Nasim <mdnasimahmed341@gmail.com>
2024-01-31 03:44:15 +0600

    Loop Lists

--- Lists.py
+++ Lists.py
@@ -79,3 +79,27 @@ print(thislist)
 thislist = ["apple", "banana", "cherry"]
 thislist.clear()
 print(thislist)
+
+
+#################Loop Lists##############
+
+## Loop List
+thislist = ["apple","banana", "cherry"]
+for x in thislist:
+    print(x)
+
+##Loop Through the Index Numbers
+thislist = ["apple", "banana", "cherry"]
+for i in range(len(thislist)):
+    print(thislist[i])
+
+## Using a While Loop
+thislist = ["apple", "cherry", "banana"]
+i = 0
+while i < len(thislist):
+    print(thislist[i])
+    i = i+1
+
+ ##Looping Using List Comprehension{short syntax}
+    thislist = ["apple", "banana", "cherry"]
+    [print(x) for x in thislist]

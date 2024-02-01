b035bfac1d5894dfc113f60098004eef3b1c80df
Nasim <mdnasimahmed341@gmail.com>
2024-02-02 04:27:41 +0600

    Update Tuples

--- Tuples.py
+++ Tuples.py
@@ -6,3 +6,9 @@ print(NewTuples[-1])
 NewTuples = (1,2,3,4,5,6,7,8)
 print(NewTuples[2:4])
  
+########### Update Tuples ###########
+thisTuple = ("Python", "Programming", "Language")
+a = list(thisTuple)
+a.append("practice")
+thisTuple = tuple(a)
+print(thisTuple)
